0a963dd1afc1fc6a37aff66aed3812015e60bc6f
H-f-society <1915140@gmail.com>
2023-04-18 20:02:15 +0800

    update

--- FSAlgo-core/src/main/java/com/fsalgo/core/tree/BPlusTree.java
+++ FSAlgo-core/src/main/java/com/fsalgo/core/tree/BPlusTree.java
@@ -1,10 +1,30 @@
 package com.fsalgo.core.tree;
 
+import java.util.List;
+
 /**
  * @Author: root
  * @Date: 2023/1/5 9:15
  * @Description: B+树
  */
-public class BPlusTree<T extends Comparable<T>> {
+public class BPlusTree<T extends Comparable<T>, V> {
+
+    public static class Node<T> {
+        boolean leaf;
+        List<T> keys;
+        List<Node<T>> child;
+        Node<T> next;
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
