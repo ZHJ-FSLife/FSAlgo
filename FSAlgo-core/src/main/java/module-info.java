0f5766063eabaf9a7dcc969ade9f122f0acdb44a
H-f-society <1915140@gmail.com>
2023-06-02 09:08:24 +0800

    update package-info.java

---
+++ FSAlgo-core/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module com.fsalgo.core {
+
+    exports com.fsalgo.core.clustering;
+    exports com.fsalgo.core.graph;
+    exports com.fsalgo.core.graph.scoring;
+    exports com.fsalgo.core.graph.shortestpath;
+    exports com.fsalgo.core.graph.spanningtree;
+    exports com.fsalgo.core.graph.strongconn;
+    exports com.fsalgo.core.interfaces;
+    exports com.fsalgo.core.math.geometrical;
+    exports com.fsalgo.core.math.linear;
+    exports com.fsalgo.core.struct;
+    exports com.fsalgo.core.struct.builder;
+    exports com.fsalgo.core.struct.specific;
+    exports com.fsalgo.core.tree;
+    exports com.fsalgo.core.tree.heap;
+    exports com.fsalgo.core.tree.heap.specific;
+    exports com.fsalgo.core.tree.vectorspace;
+    exports com.fsalgo.core.tree.vectorspace.specific;
+    exports com.fsalgo.core.util;
+    exports com.fsalgo.core.util.sort;
+
+}
