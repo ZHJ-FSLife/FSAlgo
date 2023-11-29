cb438543d1880c39e8652e761b30039dc243b6c2
H-f-society <1915140@gmail.com>
2023-11-29 16:29:10 +0800

    update

--- FSAlgo-core/src/main/java/com/fsalgo/core/tree/RadixTree.java
+++ FSAlgo-core/src/main/java/com/fsalgo/core/tree/RadixTree.java
@@ -1,3 +1,22 @@
+/*
+ * FSAlgo
+ * https://github.com/H-f-society/FSAlgo
+ *
+ * Copyright (C) [2023] [ZhongHongJie]
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.fsalgo.core.tree;
 
 import com.fsalgo.core.interfaces.NameEntity;
@@ -29,6 +48,7 @@ public class RadixTree implements NameEntity {
 
     public void add(Node node, String word) {
         int matchLength = node.matchLength(word);
+        // if (node == null)
     }
 
     public int size() {
--- FSAlgo-core/src/main/java/com/fsalgo/core/tree/heap/AbstractHeap.java
+++ FSAlgo-core/src/main/java/com/fsalgo/core/tree/heap/AbstractHeap.java
@@ -1,3 +1,22 @@
+/*
+ * FSAlgo
+ * https://github.com/H-f-society/FSAlgo
+ *
+ * Copyright (C) [2023] [ZhongHongJie]
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.fsalgo.core.tree.heap;
 
 /**
