ae64e13553b287de44d9bacbf6e91e56f48dd862
ZHJ-FSLife <1915140@gmail.com>
2024-01-18 15:53:22 +0800

    update

--- FSAlgo-core/src/main/java/com/fsalgo/core/constant/BaseConstant.java
+++ FSAlgo-core/src/main/java/com/fsalgo/core/constant/BaseConstant.java
@@ -24,11 +24,12 @@ package com.fsalgo.core.constant;
  * @Date: 2023/6/30 12:30
  * @Description: 基本常数
  */
-public interface BaseConstant {
+public class BaseConstant {
 
+    private BaseConstant() { }
     /**
      * 序列化id
      */
-    long SERIAL_VERSION_UID = 1L;
+    public static final long SERIAL_VERSION_UID = 1L;
 
 }
